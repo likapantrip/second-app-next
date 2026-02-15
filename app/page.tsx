e230cb420b478353573f1fb57c7c59f74ef520ce
likapantrip <likapantrip@gmail.com>
2026-02-15 21:54:47 +0900

    余白コードの修正

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <h2>トップページ</h2>
       <p>Welcome to Second App</p>
-      <br></br>
+      <br />
       <Link href="/create">新規作成画面へ</Link>
     </div>
   );
